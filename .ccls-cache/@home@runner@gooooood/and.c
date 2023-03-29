024b7c26e75788305a16b22b97d5a3a13fb7b9da
31112 <128555383+kdkddd@users.noreply.github.com>
2023-03-29 00:24:37 +0000

    AND 연산을 배웠다.

--- .ccls-cache/@home@runner@gooooood/and.c
+++ .ccls-cache/@home@runner@gooooood/and.c
@@ -1,9 +1,9 @@
 
-// int main(void)
-// {
-//   int a=15;
-//   int b=20;
-//   int c = a&b;
+int main(void)
+{
+  int a=15;
+  int b=20;
+  int c = a&b;
 
-//   printf("AND 연산 결과 : %d", c);
-// }
+  printf("AND 연산 결과 : %d", c);
+}
