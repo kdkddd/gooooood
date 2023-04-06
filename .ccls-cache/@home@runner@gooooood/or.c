b50a61cafc929f69ed94545b3210897a4185ecd0
31112 <>
2023-04-06 04:59:34 +0000

    if

--- .ccls-cache/@home@runner@gooooood/or.c
+++ .ccls-cache/@home@runner@gooooood/or.c
@@ -1,8 +1,8 @@
-int main(void)
-{
-  int a=15;
-  int b=20;
-  int c = a|b;
+// int main(void)
+// {
+//   int a=15;
+//   int b=20;
+//   int c = a|b;
 
-  printf("OR 연산 결과 : %d", c);
-}
+//   printf("OR 연산 결과 : %d", c);
+// }
