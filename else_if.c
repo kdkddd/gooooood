1077dc5c0fab2797f2f715b1f19ec4a3b7d44da7
kdkddd <128555383+kdkddd@users.noreply.github.com>
2023-05-24 00:24:03 +0000

    while_구구단

--- else_if.c
+++ else_if.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-main(){
-  int a;
-  printf("(1)사과 | (2)키위 | (3)복숭아 | (4)수박\n");
-  printf("좋아하는 과일은?");
-  scanf("%d", &a);
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("(1)사과 | (2)키위 | (3)복숭아 | (4)수박\n");
+//   printf("좋아하는 과일은?");
+//   scanf("%d", &a);
   
-  if(a==1)
-    printf("사과를 좋아하는구나!");
+//   if(a==1)
+//     printf("사과를 좋아하는구나!");
 
-  else if(a==2)
-   printf("키위를 좋아하는구나!");
+//   else if(a==2)
+//    printf("키위를 좋아하는구나!");
 
-  else if(a==3)
-   printf("복숭아를 좋아하는구나!");
+//   else if(a==3)
+//    printf("복숭아를 좋아하는구나!");
 
-  else if(a==4)
-   printf("수박을 좋아하는구나!");
+//   else if(a==4)
+//    printf("수박을 좋아하는구나!");
 
-  else
-      printf("넌 아마 과일을 좋아하지 않는 것 같아!");
+//   else
+//       printf("넌 아마 과일을 좋아하지 않는 것 같아!");
 
-printf("좋아하는 과일 조사 끝~:)");
-}
+// printf("좋아하는 과일 조사 끝~:)");
+// }
---
+++ mission_myscore.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("점수를 입력하세요");
+//   scanf("%d", &a);
+  
+//   if(a>=90)
+//     printf("A");
+
+//   else if(a>=80)
+//     printf("B");
+
+//   else if(a>=70)
+//     printf("C");
+
+//   else if(a>=60)
+//     printf("D");
+
+//   else if(a>=0)
+//     printf("F");
+
+// printf("학점 출력 끝");
+// }
